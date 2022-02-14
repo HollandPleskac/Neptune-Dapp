48076e8d5a40296f1be8f3a70b540c8b6b31bef6
Faith Morante <faith.morante@gmail.com>
2022-02-14 16:19:58 +0800

    * use Inter font * use root as baseUrl * add dropshadow on deposits/borrowed graph * add prettier settings * fonts styling in lending page

--- frontend_ui/components/common/Navbar/index.tsx
+++ frontend_ui/components/common/Navbar/index.tsx
@@ -1,13 +1,32 @@
 import WhiteLogo from './../../../assets/WhiteLogo'
 const Navbar = () => {
+  const links = [
+    {
+      name: "Dashboard",
+      link: "/",
+    },
+    {
+      name: "Lend",
+      link: "/",
+    },
+    {
+      name: "Stake",
+      link: "/",
+    },
+    {
+      name: "Governance",
+      link: "/",
+    },
+  ]
   return (
     <nav className="flex justify-between text-white items-center mt-6 mb-14">
       <WhiteLogo />
       <ul className='flex justify-between'>
-        <li className='mr-6'>Dashboard</li>
-        <li className='mr-6'>Lend</li>
-        <li className='mr-6'>Stake</li>
-        <li>Governance</li>
+        {
+          links.map((link, i) => (
+            <li key={i} className='mr-6 font-bold'>{link.name}</li>
+          ))
+        }
       </ul>
       <p>Wallet</p>
     </nav>
