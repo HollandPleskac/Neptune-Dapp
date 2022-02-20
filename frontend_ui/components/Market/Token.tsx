13705fdbababba0fca7afd2fcca0fc54afed3944
Holland Pleskac <hollandpleskac@gmail.com>
2022-02-20 10:55:16 -0800

    refactored props

--- frontend_ui/components/Market/Token.tsx
+++ frontend_ui/components/Market/Token.tsx
@@ -1,24 +1,28 @@
-const Token: React.FC<{
+import NeptuneLogo from 'assets/NeptuneLogo';
+
+const Token = ({ name, lendAPY, rewardsAPR, totalLent }: Props) => {
+  return (
+    <div className='flex items-center bg-dark-secondary py-6 mb-4 rounded-2xl'>
+      <div className='flex items-center text-sm font-bold w-3/12 ml-6'>
+        <div className='w-8 h-8 bg-blue-600 rounded-full'></div>
+        <h3 className='ml-4'>{name}</h3>
+      </div>
+      <div className='w-2/12 text-sm font-bold'>{lendAPY}</div>
+      <div className='flex-1 flex items-center text-sm font-bold'>
+        {rewardsAPR}
+        {/* <div className='w-4 h-4 ml-1 bg-blue-600 rounded-full mr-2'></div> */}
+        <NeptuneLogo className='w-4 h-4 ml-1' />
+      </div>
+      <div className='flex justify-end mr-6 text-sm font-bold'>{totalLent}</div>
+    </div>
+  );
+};
+
+type Props = {
   name: string;
   lendAPY: string;
   rewardsAPR: string;
   totalLent: string;
-}> = (props) => {
-  return (
-    <div className='flex items-center bg-dark-secondary py-6 mb-4 rounded-2xl'>
-      <div className='text-sm font-bold w-3/12 ml-6 flex items-center'>
-        <div className='w-8 h-8 bg-blue-600 rounded-full'></div>
-        <h3 className='ml-4'>{props.name}</h3>
-      </div>
-      <div className='w-2/12 text-sm font-bold'>{props.lendAPY}</div>
-      <div className='flex-1 flex items-center text-sm font-bold'>
-        {props.rewardsAPR}
-        <div className='w-4 h-4 ml-1 bg-blue-600 rounded-full mr-2'></div>
-      </div>
-      <div className='flex justify-end mr-6 text-sm font-bold'>
-        {props.totalLent}
-      </div>
-    </div>
-  );
 };
+
 export default Token;
--- frontend_ui/components/Market/TokenLabels.tsx
+++ frontend_ui/components/Market/TokenLabels.tsx
@@ -1,15 +1,20 @@
-const TokenLabels: React.FC<{ isLend: boolean }> = (props) => {
+const TokenLabels = ({ isLend }: Props) => {
   return (
     <div className='flex mb-4'>
       <h4 className='w-3/12 mr-6 text-sm text-gray-faded'>Token</h4>
       <h4 className='w-2/12 text-sm text-gray-faded'>
-        {props.isLend ? 'Lend' : 'Borrow'} APY
+        {isLend ? 'Lend' : 'Borrow'} APY
       </h4>
       <h4 className='flex-1 text-sm text-gray-faded'>Rewards APR</h4>
       <h4 className='flex justify-end mr-6 text-sm text-gray-faded'>
-        Total {props.isLend ? 'Lent' : 'Borrowed'}
+        Total {isLend ? 'Lent' : 'Borrowed'}
       </h4>
     </div>
   );
 };
+
+type Props = {
+  isLend: boolean;
+};
+
 export default TokenLabels;
