297cef05e7699ee3de67f7418966e385958848a2
Holland Pleskac <hollandpleskac@gmail.com>
2022-02-20 11:27:52 -0800

    routing for token

--- frontend_ui/components/Market/Token.tsx
+++ frontend_ui/components/Market/Token.tsx
@@ -1,17 +1,20 @@
 import NeptuneLogo from 'assets/NeptuneLogo';
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/router';
 import React from 'react';
 
 const Token = ({ name, lendAPY, rewardsAPR, totalLent, href }: Props) => {
-  const router = useRouter()
+  const router = useRouter();
 
   const handleClick = (e: React.MouseEvent) => {
-    e.preventDefault()
-    router.push(href)
-  }
+    e.preventDefault();
+    router.push(href);
+  };
 
   return (
-    <div className='flex items-center bg-dark-secondary py-6 mb-4 rounded-2xl cursor-pointer' onClick={handleClick}>
+    <div
+      className='flex items-center bg-dark-secondary py-6 mb-4 rounded-2xl cursor-pointer'
+      onClick={handleClick}
+    >
       <div className='flex items-center text-sm font-bold w-3/12 ml-6'>
         <div className='w-8 h-8 bg-blue-600 rounded-full'></div>
         <h3 className='ml-4'>{name}</h3>
