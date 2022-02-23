54db4e7c8a21f5bba5a56242b01fe6bba63379d6
Holland Pleskac <hollandpleskac@gmail.com>
2022-02-22 19:37:43 -0800

    Link instead of useRouter

--- frontend_ui/components/Market/Token.tsx
+++ frontend_ui/components/Market/Token.tsx
@@ -1,34 +1,26 @@
 import NeptuneLogo from 'assets/NeptuneLogo';
-import { useRouter } from 'next/router';
 import React from 'react';
+import Link from 'next/link';
 
 const Token = ({ name, APY, rewardsAPR, totalAmount, href }: Props) => {
-  const router = useRouter();
-
-  const handleClick = (e: React.MouseEvent) => {
-    e.preventDefault();
-    router.push(href);
-  };
-
   return (
-    <div
-      className='flex items-center bg-dark-secondary py-6 mb-4 rounded-2xl cursor-pointer'
-      onClick={handleClick}
-    >
-      <div className='flex items-center text-sm font-bold w-3/12 ml-6'>
-        <div className='w-8 h-8 bg-blue-600 rounded-full'></div>
-        <h3 className='ml-4'>{name}</h3>
-      </div>
-      <div className='w-2/12 text-sm font-bold'>{APY}</div>
-      <div className='flex-1 flex items-center text-sm font-bold'>
-        {rewardsAPR}
-        {/* <div className='w-4 h-4 ml-1 bg-blue-600 rounded-full mr-2'></div> */}
-        <NeptuneLogo className='w-4 h-4 ml-1' />
-      </div>
-      <div className='flex justify-end mr-6 text-sm font-bold'>
-        {totalAmount}
-      </div>
-    </div>
+    <Link href={href}>
+      <a className='flex items-center bg-dark-secondary py-6 mb-4 rounded-2xl cursor-pointer'>
+        <div className='flex items-center text-sm font-bold w-3/12 ml-6'>
+          <div className='w-8 h-8 bg-blue-600 rounded-full'></div>
+          <h3 className='ml-4'>{name}</h3>
+        </div>
+        <div className='w-2/12 text-sm font-bold'>{APY}</div>
+        <div className='flex-1 flex items-center text-sm font-bold'>
+          {rewardsAPR}
+          {/* <div className='w-4 h-4 ml-1 bg-blue-600 rounded-full mr-2'></div> */}
+          <NeptuneLogo className='w-4 h-4 ml-1' />
+        </div>
+        <div className='flex justify-end mr-6 text-sm font-bold'>
+          {totalAmount}
+        </div>
+      </a>
+    </Link>
   );
 };
 
