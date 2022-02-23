8e7ea41ee211c02663a58e4d1b1d35c4c6a35da3
Holland Pleskac <hollandpleskac@gmail.com>
2022-02-22 20:23:12 -0800

    spacing on tokens and arrows

---
+++ frontend_ui/components/Market/Label.tsx
@@ -0,0 +1,30 @@
+import DescendingSortArrow from '../../assets/DescendingSortArrow';
+import AscendingSortarrow from '../../assets/AscendingSortarrow';
+
+const Label = ({ name, widthClasses, field, sortFn }: Props) => {
+  return (
+    <div className={`flex items-center ${widthClasses}`}>
+      <div
+        className='flex items-center cursor-pointer'
+        onClick={() => {
+          sortFn(field);
+        }}
+      >
+        <h4 className='mr-3 text-sm text-gray-faded'>{name}</h4>
+        <div>
+          <AscendingSortarrow className='mb-[0.125rem]' />
+          <DescendingSortArrow />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+type Props = {
+  name: string;
+  widthClasses: string;
+  field: string;
+  sortFn: (field: string) => void;
+};
+
+export default Label;
--- frontend_ui/components/Market/TokenLabels.tsx
+++ frontend_ui/components/Market/TokenLabels.tsx
@@ -1,39 +1,39 @@
+import Label from './Label';
+
 const TokenLabels = ({ isLend, sortFn }: Props) => {
   const labels = [
     {
       name: 'Token',
-      className: 'w-3/12 mr-6 text-sm text-gray-faded cursor-pointer',
+      widthClasses: 'w-[164px] pl-6',
       field: 'name',
     },
     {
       name: `${isLend ? 'Lend' : 'Borrow'} APY`,
-      className: 'w-2/12 text-sm text-gray-faded cursor-pointer',
+      widthClasses: 'w-[116px]',
       field: 'APY',
     },
     {
       name: 'Rewards APR',
-      className: 'flex-1 text-sm text-gray-faded cursor-pointer',
+      widthClasses: 'flex-1',
       field: 'rewardsAPR',
     },
     {
       name: `Total ${isLend ? 'Lent' : 'Borrowed'} `,
-      className: 'flex justify-end mr-6 text-sm text-gray-faded cursor-pointer',
-      field: 'total',
+      widthClasses: 'justify-end mr-6',
+      field: 'totalAmount',
     },
   ];
 
   return (
     <div className='flex mb-4'>
       {labels.map((label, i) => (
-        <h4
+        <Label
           key={i}
-          className={label.className}
-          onClick={() => {
-            sortFn(label.field);
-          }}
-        >
-          {label.name}
-        </h4>
+          name={label.name}
+          widthClasses={label.widthClasses}
+          field={label.field}
+          sortFn={sortFn}
+        />
       ))}
     </div>
   );
