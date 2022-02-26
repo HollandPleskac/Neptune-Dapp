d8cf52ae766dc1ca085dec44520e6a9feb38c2f2
Faith Morante <faith.morante@gmail.com>
2022-02-26 18:57:22 +0800

    minor fe changes * hide input number arrows * tab bg color to be blue * remove gear icon * placeholder should be 0 * fix line on graph on left * left details update * NPT to NEP * Deposit to Lend * button hover * add ltv to repay

--- frontend_ui/components/LendAndBorrow/Stats/TopDetails/index.tsx
+++ frontend_ui/components/LendAndBorrow/Stats/TopDetails/index.tsx
@@ -1,33 +1,43 @@
+import cx from 'classnames';
 import TopStats from './TopStats';
 import Graph from './Graph';
 import styles from './topDetails.module.scss';
 const TopDetails = () => {
   return (
-    <div className='bg-dark-secondary rounded-2xl p-8 flex flex-col justify-between'>
+    <div
+      className={cx(
+        'bg-dark-secondary rounded-2xl p-8 flex flex-col justify-between',
+        styles['neptune-top-details'],
+      )}
+    >
       <TopStats />
 
       <hr className={styles['neptune-top-details__hr']} />
 
       <div className={styles['neptune-top-details__graph-wrapper']}>
-        <div style={{ marginRight: '3rem' }}>
+        <div
+          style={{ marginRight: '3rem' }}
+          className='flex flex-col justify-between items-start'
+        >
           <span className='text-xs leading-3 font-medium opacity-50'>
-            Total Deposits
+            Total Lent
           </span>{' '}
-          <br />
-          <span className='font-bold text-base leading-5'>$3,456,069</span>{' '}
-          <hr className='w-2 opacity-50 my-3' />
+          <span className='font-bold text-base leading-5 mt-3'>$3,456,069</span>{' '}
+          <span className='text-right block hr' />
           <span className='text-xs leading-3 font-medium opacity-50'>
             18,778.90 SOL
           </span>
         </div>
         <Graph />
-        <div style={{ marginLeft: '3rem' }}>
+        <div
+          style={{ marginLeft: '3rem' }}
+          className='flex flex-col justify-between items-end'
+        >
           <span className='text-xs leading-3 font-medium opacity-50'>
             Total Borrowed
           </span>{' '}
-          <br />
-          <span className='font-bold text-base leading-5'>$1,456,069</span>{' '}
-          <hr className='w-2 opacity-50 my-3' />
+          <span className='font-bold text-base leading-5 mt-3'>$1,456,069</span>{' '}
+          <span className='text-right block hr' />
           <span className='text-xs leading-3 font-medium opacity-50'>
             7,911.70 SOL
           </span>
